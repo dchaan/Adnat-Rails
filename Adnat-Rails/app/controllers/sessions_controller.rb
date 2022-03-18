7ac11d4bc27d79409fe4b8102e2747e9b6a56b59
David Chan <dchaan1@gmail.com>
2022-03-18 11:49:30 -0700

    Sessions controller

---
+++ Adnat-Rails/app/controllers/sessions_controller.rb
@@ -0,0 +1,43 @@
+class SessionsController < ApplicationController
+  skip_before_action :auth, only: [:new, :create]
+
+  def new
+    redirect_to root_path if session[:user_id]
+  end
+
+  def create
+    user = User.find_by(:email, params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      if user.organization_id == nil
+        redirect_to welcome_path
+      else
+        redirect_to overview_path
+      end
+    else
+      flash.now.alert = "Invalid email or password!"
+      render :new
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to login_path
+  end
+
+  def welcome
+    if current_user.organization_id == nil
+      @organization = Organization.new
+    else
+      redirect_to welcome_path
+    end
+  end
+
+  def overview
+    if current_user.organization_id == nil
+      @organization= Organization.find(current_user.organization_id)
+    else
+      redirect_to overview_path
+    end
+  end
+end
