762d9d50a1cfc7f068937816b0b682c36677943c
David Chan <dchaan1@gmail.com>
2022-03-18 11:55:30 -0700

    App controller

--- Adnat-Rails/app/controllers/application_controller.rb
+++ Adnat-Rails/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :auth
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def auth
+    redirect_to login_path if !logged_in?
+  end
 end
