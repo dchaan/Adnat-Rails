2088eda8fa4ea20b669a9594a43b2b8a8b4000dc
David Chan <dchaan1@gmail.com>
2022-03-18 13:17:10 -0700

    Users controller

--- Adnat-Rails/app/controllers/users_controller.rb
+++ Adnat-Rails/app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+  skip_before_action :auth, :only[:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to overview_path
+    else
+      redirect_to welcome_path
+    end
+  end
+
+  def join_org
+    @organization = Organization.find(params[:id])
+    User.update(current_user.id, organization_id: @organization.id)
+    redirect_to overview_path
+  end
+
+  def leave_org
+    User.update(current_user.id, organization_id: nil)
+    redirect_to welcome_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
