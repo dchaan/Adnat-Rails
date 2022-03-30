ef972b1cc07d966c2df84e2488668d46db808a88
David Chan <dchaan1@gmail.com>
2022-03-30 10:29:04 -0700

    Add comments

--- Adnat-Rails/app/controllers/shifts_controller.rb
+++ Adnat-Rails/app/controllers/shifts_controller.rb
@@ -61,9 +61,11 @@ class ShiftsController < ApplicationController
 
   # calc shift length
   def get_shift_duration(start, finish)
+    # if shift starts and finishes on same day
     if start < finish
       (finish - start) / 60 / 60
     else 
+      # if shift finishes the following day
       (finish.next_day() - start)/ 60 / 60
     end
   end
@@ -72,6 +74,7 @@ class ShiftsController < ApplicationController
   def get_shift_cost(start, finish, hours, rate, break_hrs)
     sun_hours = 0
     norm_hours = 0
+
     if start < finish
       if start.sunday?
         sun_hours = hours
@@ -79,13 +82,16 @@ class ShiftsController < ApplicationController
         norm_hours = hours
       end
     else
+      # if shift finishes the following day
       real_finish = finish.next_day() - (60 * 60 * break_hrs)
-      # sunday shift cost
+      # if shift starts on sunday and finishes on monday
       if start.sunday? && real_finish.monday?
         sun_hours = (start.seconds_until_end_of_day + 1) / 60 / 60
+      # if shift starts on saturday and finishes on sunday
       elsif start.saturday? && real_finish.sunday?
         sun_hours = real_finish.seconds_since_midnight / 60 / 60
       end
+      # subtract sunday hours from hours to get normal hours
       norm_hours = hours - sun_hours
     end
     return (norm_hours * rate) + (2 * sun_hours * rate)
@@ -93,9 +99,11 @@ class ShiftsController < ApplicationController
 
   # display shift details, including date, start time, finish time, break length, hours worked, shift cost
   def shift_details
+    # objects to store names, hours_worked, and shift_costs
     @names = {}
     @hours_worked = {}
     @shift_costs = {}
+  
     @organization = Organization.find(current_user.organization_id)
 
     # query shifts for current org
@@ -106,10 +114,15 @@ class ShiftsController < ApplicationController
 
     # iterate through shifts in current org
     @shifts.each do |shift|
+      # break length in minutes
       break_l = Float(shift.break_length) / 60
+      # shift length
       shift_duration = get_shift_duration(shift.start, shift.finish)
+      # shift length - break length
       hours_worked = shift_duration - break_l
+      # shift cost
       shift_cost = get_shift_cost(shift.start, shift.finish, hours_worked, @organization.hourly_rate, break_l)
+      
       @names[shift] = User.find(shift.user_id).name
       @hours_worked[shift] = hours_worked.round(2)
       @shift_costs[shift] = shift_cost.round(2)
